8c17cee008c4ec2258b01a46a7eeb9e0562ab951
Kai <kai.hollberg@googlemail.com>
2015-06-13 00:41:35 +0200

    first steps

--- beetsplug/printimport.py
+++ beetsplug/printimport.py
@@ -1,4 +1,9 @@
 from beets.plugins import BeetsPlugin
 
 class PrintImport(BeetsPlugin):
-    pass
+    def __init__(self):
+        super(PrintImport, self).__init__()
+        self.register_listener('album_imported', self.printimport)
+
+    def printimport(library, album):
+        print u'Musik: ' + album.albumartist + ' - ' + album.album + ' (' + album.genre + ' - ' + album.year + ') @ ' + album.bitrate + ' kBit/s'
